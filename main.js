d0481bc00c9e271047465ddafa138131a517e1ed
amenpunk <ondasycircuitos@gmail.com>
2018-07-07 18:29:51 -0600

    funciones basicas

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 function insertar(num){
     document.form.textview.value = document.form.textview.value+num;
 }
+
+function igual(){
+    var exp = document.form.textview.value;
+    
+    if(exp){
+        document.form.textview.value = eval(exp);
+    }
+}
+
+function limp(){
+    document.form.textview.value ="";
+}
+
+function playSound() {
+    var sound = document.getElementById("audio");
+    sound.play();
+}
